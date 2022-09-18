5f2f856bba070689826a4ce7621d75817f1dba99
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-09-18 20:03:14 +0300

    Create mysql_create_db.sql

---
+++ SetupScripts/mysql_create_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `persons`.`persons` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `age` INT NULL,
+  `address` VARCHAR(45) NULL,
+  `work` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
